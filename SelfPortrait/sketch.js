9be1a017d17848ca84496d95275c523491417c07
Seba11001 <smiezgiel@gmail.com>
2018-09-06 14:50:13 -0400

    Added self portrait

---
+++ SelfPortrait/sketch.js
@@ -0,0 +1,24 @@
+/*Self Portrait V.1 by Sebastian*/
+
+function setup() {
+    createCanvas(640, 360);
+}
+
+function draw ()  {
+    background(100);
+    
+    fill("tan");
+    ellipse(300, 180, 300);
+    
+    fill("white");
+    ellipse(200, 150, 50);
+    ellipse(380, 150, 50);
+    fill("green")
+    ellipse(200, 150, 35);
+    ellipse(380, 150, 35);
+    fill("black");
+    ellipse(200, 150, 20);
+    ellipse(380, 150, 20);
+    fill("red")
+    rect(250, 230, 70, 20, 10);
+} 
