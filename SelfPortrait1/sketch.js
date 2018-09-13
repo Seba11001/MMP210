45a49d7a845d92346026398edcb5a1a998d43eb4
Seba11001 <smiezgiel@gmail.com>
2018-09-13 15:22:42 -0400

    added semicolons

--- SelfPortrait1/sketch.js
+++ SelfPortrait1/sketch.js
@@ -9,58 +9,60 @@ function setup() {
 function draw ()  {
     background(100);
     
-    var faceSize = 300
-    var eyeSize = 50
-    var eyecolorSize = 35
-    var pupilSize = 15
-    var lefteyex = 200
-    var eye = 150
-    var righteyex = 380
-    var hairColor = "yellow"
-    var skinColor = "tan"
-    var headx = 300
-    var heady = 180
-    var hairx = 300
-    var hairy = 150
-    var eyeballcolor = "white"
-    var eyecolor = "green"
-    var pupilcolor = "black"
-    var mouthcolor = "red"
-    var mouthx = 250
-    var mouthy = 230
-    var mouthlength = 70
-    var mouthwidth = 20
-    var mouthcurve = 10
-    var nosex = 280
-    var nosey = 180
-    var noselength = 10
-    var nosewidth = 20
+    var x = 5;
+    
+    var faceSize = 300;
+    var eyeSize = 50;
+    var eyecolorSize = 35;
+    var pupilSize = 15;
+    var lefteyex = 200;
+    var eyeY = 150;
+    var righteyex = 380;
+    var hairColor = "yellow";
+    var skinColor = "tan";
+    var headx = 300;
+    var heady = 180;
+    var hairx = 300;
+    var hairy = 150;
+    var eyeballcolor = "white";
+    var eyecolor = "green";
+    var pupilcolor = "black";
+    var mouthcolor = "red";
+    var mouthx = 250;
+    var mouthy = 230;
+    var mouthlength = 70;
+    var mouthwidth = 20;
+    var mouthcurve = 10;
+    var nosex = 280;
+    var nosey = 180;
+    var noselength = 10;
+    var nosewidth = 20;
     
     
     //hair
     fill(hairColor);
-    ellipse(hairx, hairy, faceSize);    
+    ellipse(hairx + x, hairy, faceSize);    
     
     //head
     fill(skinColor);
-    ellipse(headx, heady, faceSize);
+    ellipse(headx + x, heady, faceSize);
     
     //eyes
     fill(eyeballcolor);
-    ellipse(lefteyex, eye, eyeSize);
-    ellipse(righteyex, eye, eyeSize);
+    ellipse(lefteyex + x, eyeY, eyeSize);
+    ellipse(righteyex + x, eyeY, eyeSize);
     fill(eyecolor)
-    ellipse(lefteyex, eye, eyecolorSize);
-    ellipse(righteyex, eye, eyecolorSize);
+    ellipse(lefteyex + x, eyeY, eyecolorSize);
+    ellipse(righteyex + x, eyeY, eyecolorSize);
     fill(pupilcolor);
-    ellipse(lefteyex, eye, pupilSize);
-    ellipse(righteyex, eye, pupilSize);
+    ellipse(lefteyex + x, eyeY, pupilSize);
+    ellipse(righteyex + x, eyeY, pupilSize);
     
     //mouth
     fill(mouthcolor)
-    rect(mouthx, mouthy, mouthlength, mouthwidth, mouthcurve);
+    rect(mouthx + x, mouthy, mouthlength, mouthwidth, mouthcurve);
     
     //nose
     fill(skinColor)
-    rect(nosex, nosey, noselength, nosewidth);
+    rect(nosex + x, nosey, noselength, nosewidth);
 } 
