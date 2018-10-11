f1d1a27a79d7df46b299851cb994cc7475d34eee
Seba11001 <smiezgiel@gmail.com>
2018-10-11 15:07:08 -0400

    improvements

--- meme1/sketch.js
+++ meme1/sketch.js
@@ -18,22 +18,25 @@ function draw ()  {
    
     fill(255, mouseX - 255, 0);
  
+    
+    fill("white")
+    if (mouseX > width/2 && mouseY > height/2) {
+	fill("red")
+	} else if (mouseX < width/2 && mouseY > height/2) {
+    textSize(50)
+    text("oh no!", 50, 450)
+	} else if (mouseX > width/2 && mouseY < height/2) {
+        fill("red")
+         rect(50, 50, 480, 480);
+	} else {
+	   textSize(40)
+        text("Panic!", 180, 130)
+        
+	}
     image(cat, 50, 50, 480, 480);
     fill("white")
     textSize(17);
     textFont("Arial Black")
     text("When you forget your homework at home", 75, 80);
-    fill("white")
-    if (mouseX > width/2 && mouseY > height/2) {
-	textSize(40)
-        text("Panic!", 180, 130)
-	} else if (mouseX < width/2 && mouseY > height/2) {
-    textSize(50)
-    text("oh no!", 50, 450)
-	} else if (mouseX > width/2 && mouseY < height/2) {
-        text("help me!", 180, 130)
-	} else {
-	   text("fiddlesticks!", 50, 450);
-	}
 } 
 
