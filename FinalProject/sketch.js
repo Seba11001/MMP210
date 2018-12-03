a0f3b3221782e312fb452eaa2eb6c8bcee4b0bd1
Seba11001 <smiezgiel@gmail.com>
2018-12-03 14:00:29 -0500

    added controls kinda

--- FinalProject/sketch.js
+++ FinalProject/sketch.js
@@ -1,34 +1,45 @@
-
-
-var cat;
-var r, g, b;
-var x = 360;
-var x2 = 360;
-var x3 = 360;
-var x4 = 360;
+var x = 320;
+var x2 = 330;
+var x3 = 340;
+var x4 = 350;
 var x5 = 360;
-var x6 = 360;
+var x6 = 370;
+var x7 = 360;
+var x8 = 360;
+var x9 = 350;
 var y = 360;
 var y2 = 360;
 var y3 = 360;
 var y4 = 360;
 var y5 = 360;
+var y6 = 360;
+var y7 = 360;
+var y8 = 360;
 var s = 5;
 var scolor = "yellow";
 var s2 = 350;
+var speedX = 5;
+var speedY = 5;
+
+
 function preload() {
 
 }
 function setup() {
     fill("black")
     createCanvas(720, 720);
-    r = random(0,255);
-	g = random(0,255);
-	b = random(0,255);
     noStroke();
 }
 
-
+function keyPressed() {
+    if (key = "z") {
+        speedX += 2;
+        speedY += 2;
+    } else if (key = "m") {
+        speedX -= 2;
+        speedY -= 2;
+    }
+}
 
  
 function draw ()  {
@@ -37,10 +48,10 @@ function draw ()  {
     
     fill("white")
     ellipse(x, 350, s)
-    x += 10;
+    x += speedX;
     s += 0.05;
 	if (x > 720) {
-		x = random(340, 380);
+		x = random(320, 400);
         s = 5;
 	}
     if (s > 30) {
@@ -48,10 +59,10 @@ function draw ()  {
     }
     
     ellipse(x2, 350, s)
-    x2 -= 10;
+    x2 -= speedX;
     s += 0.05;
 	if (x2 < 0) {
-		x2 = random(340, 380);
+		x2 = random(320, 400);
         s2 = 5;
 	}
     if (s2 > 30) {
@@ -59,11 +70,11 @@ function draw ()  {
     }
    
     ellipse(x3, y, s)
-    x3 -= 10;
+    x3 -= speedX;
     s += 0.05;
-    y += 10;
+    y += speedY;
 	if (x3 < 0) {
-		x3 = random(340, 380);
+		x3 = random(320, 400);
         y = 350;
         s = 5;
 	}
@@ -72,11 +83,11 @@ function draw ()  {
     }
     
     ellipse(x4, y2, s)
-    x4 += 10;
+    x4 += speedX;
     s += 0.05;
-    y2 -= 10;
+    y2 -= speedY;
 	if (x4 > 720) {
-		x4 = random(340, 380);
+		x4 = random(320, 400);
         y2 = 360;
         s = 5;
 	}
@@ -85,11 +96,11 @@ function draw ()  {
     }
     
     ellipse(x5, y3, s)
-    x5 += 10;
+    x5 += speedX;
     s += 0.05;
-    y3 += 10;
+    y3 += speedY;
 	if (x5 > 720) {
-		x5 = random(340, 380);
+		x5 = random(320, 400);
         y3 = 360;
         s = 5;
 	}
@@ -98,16 +109,41 @@ function draw ()  {
     }
     
     ellipse(x6, y5, s)
-    x6 -= 10;
+    x6 -= speedX;
     s += 0.05;
-    y5 -= 10;
+    y5 -= speedY;
 	if (x6 < 0) {
-		x6 = random(340, 380);
+		x6 = random(320, 400);
         y5 = 360;
         s = 5;
 	}
       if (s2 > 30) {
         var scolor = 0;
     }
+    
+    ellipse(x7, y6, s)
+    s += 0.05;
+    y6 -= speedY;
+	if (y6 < 0) {
+		x7 = random(320, 400);
+        y6 = 360;
+        s = 5;
+	}
+      if (s2 > 30) {
+        var scolor = 0;
+    }
+    
+    ellipse(x8, y7, s)
+    s += 0.05;
+    y7 += speedY;
+	if (y7 > 720) {
+		x8 = random(320, 400);
+        y7 = 360;
+        s = 5;
+	}
+      if (s2 > 30) {
+        var scolor = 0;
+    }
 } 
+
 
