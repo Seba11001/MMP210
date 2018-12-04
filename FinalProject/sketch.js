edcf8dd8d5bc8095ecaab85561347b58482e52f2
Seba11001 <smiezgiel@gmail.com>
2018-12-04 14:46:06 -0500

    added moon and mars

--- FinalProject/sketch.js
+++ FinalProject/sketch.js
@@ -10,6 +10,7 @@ var x9 = 350;
 var x10 = 250;
 var x11 = 450;
 var x12 = 450;
+var x13 = 400;
 var y = 360;
 var y2 = 360;
 var y3 = 360;
@@ -21,7 +22,11 @@ var y8 = 360;
 var y9 = 250;
 var y10 = 250;
 var y11 = 450;
+var y12 = 360;
 var s = 5;
+var marsSize = 0;
+var moonSize = 0;
+var bcolor = "black";
 var scolor = "yellow";
 var s2 = 350;
 var speedX = 2;
@@ -33,36 +38,35 @@ function preload() {
 
 }
 function setup() {
-    fill("black")
+    fill(bcolor);
     createCanvas(720, 720);
     noStroke();
 }
 
+//KeyboardControls
 function keyTyped() {
     if (key === "z") {
         speedX += 2;
         speedY += 2;
+        marsSize = 0;
+        moonSize = 0;
     } else if (key === "x") {
         speedX -= 1;
         speedY -= 1;
-    }
+        marsSize = 0;
+        moonSize = 0
+    }  
 }
-
 
  
 function draw ()  {
-    background("black");
-    
-    fill("white")
-   rect(l, k, w, h);
-   
-    
+    background(bcolor);
 
     if (speedX < 5) {
        s = 5;
    }
-    
-    
+   
+    //star1
     fill("white")
     ellipse(x, 350, s)
     x += speedX;
@@ -75,6 +79,7 @@ function draw ()  {
         var scolor = 0;
     }
     
+    //star2
     ellipse(x2, 350, s)
     x2 -= speedX;
     s += 0.05;
@@ -86,6 +91,7 @@ function draw ()  {
         var scolor = 0;
     }
    
+    //star3
     ellipse(x3, y, s)
     x3 -= speedX;
     s += 0.05;
@@ -99,6 +105,7 @@ function draw ()  {
         var scolor = 0;
     }
     
+    //star4
     ellipse(x4, y2, s)
     x4 += speedX;
     s += 0.05;
@@ -112,6 +119,7 @@ function draw ()  {
         var scolor = 0;
     }
     
+    //star5
     ellipse(x5, y3, s)
     x5 += speedX;
     s += 0.05;
@@ -125,6 +133,7 @@ function draw ()  {
         var scolor = 0;
     }
     
+    //star6
     ellipse(x6, y5, s)
     x6 -= speedX;
     s += 0.05;
@@ -138,6 +147,7 @@ function draw ()  {
         var scolor = 0;
     }
     
+    //star 7
     ellipse(x7, y6, s)
     s += 0.05;
     y6 -= speedY;
@@ -150,6 +160,7 @@ function draw ()  {
         var scolor = 0;
     }
     
+    //star 8
     ellipse(x8, y7, s)
     s += 0.05;
     y7 += speedY;
@@ -162,6 +173,7 @@ function draw ()  {
         var scolor = 0;
     }
     
+    //star 9
     ellipse(x10, y9, s)
     x10 -= speedX;
     s += 0.05;
@@ -175,6 +187,7 @@ function draw ()  {
         var scolor = 0;
     }
     
+    //star 10
     ellipse(x11, y10, s)
     x11 += speedX;
     s += 0.05;
@@ -188,6 +201,7 @@ function draw ()  {
         var scolor = 0;
     }
     
+    //star 11
     ellipse(x12, y11, s)
     x12 += speedX;
     s += 0.05;
@@ -201,7 +215,58 @@ function draw ()  {
         var scolor = 0;
           
     }
-} 
+    
+    //star 12
+    ellipse(x13, y12, s)
+    x13 -= speedX;
+    s += 0.05;
+    y12 += speedY;
+	if (x13 < 0) {
+		x13 = random(350, 400);
+        y12 = 360;
+        s = 5;
+	}
+      if (s2 > 30) {
+        var scolor = 0;
+      }
+    
+    
+    if (speedX < 0) {
+        speedX = 0;
+    } else if (speedY < 0) {
+        speedY = 0;
+    }
+    
+    //Mars
+    fill("red")
+        ellipse(360, 360, marsSize);
+     if (key === "m") {
+        marsSize += 3;
+         moonSize = 0;
+    } if (marsSize > 350) {
+        marsSize = 350;
+        speedX = 0;
+        speedY = 0;
+    }
+    
+    //Moon
+    fill("gray")
+        ellipse(360, 360, moonSize);
+     if (key === "l") {
+         speedX = 5;
+         speedY = 5;
+        moonSize += 3;
+         marsSize = 0;
+    } if (moonSize > 350) {
+        moonSize = 350;
+        speedX = 0;
+        speedY = 0;
+    }
+}
+
+
+
+
 
 function mousePressed() {
 	if ( mouseX > l && mouseX < l + w && mouseY > k && mouseY < k + h ) {
